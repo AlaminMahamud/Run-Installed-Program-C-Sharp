4dbb4380bfc139c8abf7ee7ff1c2565e4d44761e
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2015-09-02 15:53:51 +0600

    Uploading Project Files 'Run Installed Programs in C#'

---
+++ RunProgram/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace RunProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+
+            ProcessStart();
+        }
+
+        private void ProcessStart()
+        {
+            try
+            {
+
+                if (textBox1.Text != "")
+                {
+                    Process.Start(textBox1.Text);
+                }
+
+            }
+            catch (System.Exception)
+            {
+                try
+                {
+                    GetInstalledApps gia = new GetInstalledApps();
+                    gia.GetInstalledAppPathAndRunProgram2(textBox1.Text);
+
+
+                }
+                catch
+                {
+                    MessageBox.Show("The Name is Wrong!");
+                }
+
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                // Enter key pressed
+                ProcessStart();
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ RunProgram/GetInstalledApps.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RunProgram
+{
+    class GetInstalledApps
+    {
+        public void GetInstalledAppPathAndRunProgram(string name)
+        {
+
+            RegistryKey regKey = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Uninstall");
+            string s = FindByDisplayName(regKey, name);
+            MessageBox.Show(s);
+            if (s != "")
+                Process.Start(s);
+
+        }
+
+        public void GetInstalledAppPathAndRunProgram2(string name)
+        {
+            name = name.Trim();
+            name = name.ToLower();
+            name = name.Replace(" ", "");
+
+            switch (name)
+            {
+
+                case "shareit":
+                    if (Directory.Exists(@"C:\Program Files (x86)\Lenovo\SHAREit"))
+                        Process.Start(@"C:\Program Files (x86)\Lenovo\SHAREit\Shareit.exe");
+                    break;
+                case "chrome":
+                    if (Directory.Exists(@"C:\Program Files (x86)\Google\Chrome\Application"))
+                        Process.Start(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe");
+                    break;
+
+                case "adobereader":
+                    if (Directory.Exists(@"C:\Program Files (x86)\Adobe\Reader 9.0\Reader"))
+                        Process.Start(@"C:\Program Files (x86)\Adobe\Reader 9.0\Reader\AcroRd32.exe");
+                    break;
+                case "idm":
+                    if (Directory.Exists(@"C:\Program Files (x86)\Internet Download Manager"))
+                        Process.Start(@"C:\Program Files (x86)\Internet Download Manager\IDMan.exe");
+                    break;
+                case "word":
+                    if (Directory.Exists(@"C:\Program Files (x86)\Microsoft Office\Office15"))
+                        Process.Start(@"C:\Program Files (x86)\Microsoft Office\Office15\WINWORD.exe");
+                    break;
+                case "excel":
+                    if (Directory.Exists(@"C:\Program Files (x86)\Microsoft Office\Office15"))
+                        Process.Start(@"C:\Program Files (x86)\Microsoft Office\Office15\EXCEL.exe");
+                    break;
+                case "powerpoint":
+                    if (Directory.Exists(@"C:\Program Files (x86)\Microsoft Office\Office15"))
+                        Process.Start(@"C:\Program Files (x86)\Microsoft Office\Office15\POWERPOINT.exe");
+                    break;
+
+                case "vlc":
+                    if (Directory.Exists(@"C:\Program Files (x86)\VideoLAN\VLC"))
+                        Process.Start(@"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe");
+                    break;
+                case "mozilla":
+                    if (Directory.Exists(@"C:\Program Files (x86)\Google\Chrome\Application"))
+                        Process.Start(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe");
+                    break;
+
+                case "torrent":
+                    //Process.Start(@"C:\Program Files (x86)\Lenovo\SHAREit\Shareit.exe");
+                    break;
+                case "gom":
+                    //Process.Start(@"C:\Program Files (x86)\Lenovo\SHAREit\Shareit.exe");
+                    break;
+                case "photoshop":
+                    //Process.Start(@"C:\Program Files (x86)\Lenovo\SHAREit\Shareit.exe");
+                    break;
+                case "illustrator":
+                    //Process.Start(@"C:\Program Files (x86)\Lenovo\SHAREit\Shareit.exe");
+                    break;
+                case "kmplayer":
+                    if (Directory.Exists(@"C:\The KMPlayer"))
+                        Process.Start(@"C:\The KMPlayer\KMPLayer.exe");
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+        public static string FindByDisplayName(RegistryKey parentKey, string name)
+        {
+            name = name.ToLower();
+            string[] nameList = parentKey.GetSubKeyNames();
+            for (int i = 0; i < nameList.Length; i++)
+            {
+                RegistryKey regKey = parentKey.OpenSubKey(nameList[i]);
+                try
+                {
+                    if (regKey.GetValue("DisplayName").ToString().Contains(name))
+                    {
+                        return regKey.GetValue("InstallLocation").ToString();
+                    }
+                }
+                catch { }
+            }
+            return "";
+        }
+    }
+}
